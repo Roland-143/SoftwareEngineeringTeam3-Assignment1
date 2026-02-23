20b2b59f5cc76f98d72bbead58f2eeaaf265cd75
Motasem <motasemamereh6@gmail.com>
2026-02-23 12:59:54 -0600

    Quick Sort Implementation (ID26 Motasem)

---
+++ Julia/QuickSort.jl
@@ -0,0 +1,11 @@
+function quick_sort(arr)
+    if length(arr) <= 1
+        return arr
+    end
+
+    pivot = arr[1]
+    less = [x for x in arr[2:end] if x <= pivot]
+    greater = [x for x in arr[2:end] if x > pivot]
+
+    return vcat(quick_sort(less), [pivot], quick_sort(greater))
+end
