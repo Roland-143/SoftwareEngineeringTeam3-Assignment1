09dc61613b4c57e6401c3623f6f319158ce3d62b
Lorial <llesniew@pnw.edu>
2026-02-23 11:53:17 -0600

    Updated the main file to include user input for testing purposes

--- main.py
+++ main.py
@@ -22,48 +22,66 @@ def main():
     data1 = [100,5,10,1,11]
     data2 = [1,2,-3,4,5]
     data3 = ["k",2,1]
+
+
+    user_input = input(
+        "Enter integers separated by spaces or commas: "
+    )
+
+    # Convert input into a list (leave validation to the algorithm)
+    raw_values = user_input.replace(",", " ").split()
+    arr = []
+
+    try:
+        arr = [int(x) for x in user_input.replace(",", " ").split()]
+    except ValueError:
+        print("Invalid input")
+        exit(1)
     
     if args.algorithm == "SELECTIONSORT":
         result1 = selection_sort(data1.copy())
         result2 = selection_sort(data2.copy())
         result3 = selection_sort(data3.copy())
+        result4 = selection_sort(arr.copy())
     if args.algorithm == "MERGESORT":
         result1 = merge_sort(data1.copy())
         result2 = merge_sort(data2.copy())
         result3 = merge_sort(data3.copy())
+        result4 = merge_sort(arr.copy())
     if args.algorithm == "INSERTIONSORT":
         result1 = insertion_sort(data1.copy())
         result2 = insertion_sort(data2.copy())
         result3 = insertion_sort(data3.copy())
+        result4 = insertion_sort(arr.copy())
     if args.algorithm == "BUBBLESORT":
         result1 = bubble_sort(data1.copy())
         result2 = bubble_sort(data2.copy())
         result3 = bubble_sort(data3.copy())
+        result4 = bubble_sort(arr.copy())
     if args.algorithm == "QUICKSORT":
         result1 = quick_sort(data1.copy())
         result2 = quick_sort(data2.copy())
         result3 = quick_sort(data3.copy())
-
+        result4 = quick_sort(arr.copy())
+    
+    print("Test Array 1:")
     print("Original:", data1)
-    if result1 is not None:
-        print("Sorted:  ", result1)
-    else:
-        print("Invalid input")
+    print("Sorted:  ", result1)
     
     print("")
+    print("Test Array 2:")
     print("Original:", data2)
-    if result2 is not None:
-        print("Sorted:  ", result2)
-    else:
-        print("Invalid input")
+    print("Sorted:  ", result2)
     
     print("")
+    print("Test Array 3:")
     print("Original:", data3)
-    if result3 is not None:
-        print("Sorted:  ", result3)
-    else:
-        print("Invalid input")
+    print("Sorted:  ", result3)
 
+    print("")
+    print("User Test Array:")
+    print("Original:", arr)
+    print("Sorted:  ", result4)
 
 if __name__ == "__main__":
     main()
